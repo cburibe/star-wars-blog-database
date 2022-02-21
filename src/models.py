db6f5aed15cdac8dd8586ba97b1dcd3006156d94
cburibe <curibepulgar@gmail.com>
2022-02-21 20:26:10 +0000

    code terminado

--- src/models.py
+++ src/models.py
@@ -8,26 +8,52 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    usernamename = Column(String(250), nullable=False)
+    password = Column(Integer, primary_key=True)
+    email = Column(String(30), nullable=False)
+
+class Favorite_Characters(Base):
+    __tablename__ = 'favorite_character'
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    id_characters = Column(Integer, ForeignKey("characters.id"))
+
+class Favorite_Planets(Base):
+    __tablename__ = 'favorite_planets'
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    id_planets = Column(Integer, ForeignKey("planets.id"))
+
+class Favorite_Starships(Base):
+    __tablename__ = 'favorite_starships'
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    id_starship = Column(Integer, ForeignKey("starship.id"))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True) 
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Starship(Base):
+    __tablename__ = 'starship'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
